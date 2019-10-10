a3e8302b11a63e03729d7da3421799527471a3c5
Yoann L'hostis <yolhostis@live.fr>
2019-10-10 22:15:16 +0200

    modif' mineure textuelle

--- generator.js
+++ generator.js
@@ -25,12 +25,14 @@ for (let i = 1; i < max; i++) {
 // Parcours de values[], récupération random d'une valeur et suppression de cette valeur dans le tableau avec splice() :
 
 for (let y = 1; y <= values.length; y) { 
+
 	let numberRandom = Math.floor(Math.random() * Math.floor(values.length));
 	let random = values.splice(numberRandom, 1);
 	finalValues.push(random);
+	
 }
 console.log(finalValues);
 
 
-// Enjoy it ! ;)
+// Enjoy ! ;)
 
